083146354f47886210f3627734bf8f96153a8af7
Edison Yang <edison.cy.yang@gmail.com>
2019-11-28 22:07:15 +0000

    sum the two largest numbers in the input array

---
+++ sumLargestNumbers.js
@@ -0,0 +1,8 @@
+const sumLargestNumbers = function(data) {
+  data.sort((a, b) => a - b);
+  return data[data.length - 1] + data[data.length - 2];
+};
+
+console.log(sumLargestNumbers([1, 10]));
+console.log(sumLargestNumbers([1, 2, 3]));
+console.log(sumLargestNumbers([10, 4, 34, 6, 92, 2]));
